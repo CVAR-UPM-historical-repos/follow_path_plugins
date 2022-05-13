f943b53a6cf4d39a66484d86d5bfba3aec1abae5
perezsaura-david <perezsaura.david@gmail.com>
2022-05-13 10:56:51 +0200

    v0.1.0

--- src/follow_path_plugin_traj.cpp
+++ src/follow_path_plugin_traj.cpp
@@ -1,3 +1,39 @@
+/*!*******************************************************************************************
+ *  \file       follow_path_plugin_traj.cpp
+ *  \brief      follow_path_plugin_traj implementation file
+ *  \authors    Miguel Fernández Cortizas
+ *              Pedro Arias Pérez
+ *              David Pérez Saura
+ *              Rafael Pérez Seguí
+ *
+ *  \copyright  Copyright (c) 2022 Universidad Politécnica de Madrid
+ *              All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
 #include "follow_path_base.hpp"
 #include "as2_msgs/msg/pose_stamped_with_id.hpp"
 #include "as2_msgs/srv/send_trajectory_waypoints.hpp"
@@ -13,6 +49,7 @@ namespace follow_path_plugins
         using YAW_MODE = as2_msgs::msg::TrajectoryWaypointsWithID;
         using SyncSetSpeed = as2::SynchronousServiceClient<as2_msgs::srv::SetSpeed>;
         using SyncSendTrajWayp = as2::SynchronousServiceClient<as2_msgs::srv::SendTrajectoryWaypoints>;
+
     public:
         rclcpp_action::GoalResponse onAccepted(const std::shared_ptr<const as2_msgs::action::FollowPath::Goal> goal) override
         {
@@ -70,8 +107,8 @@ namespace follow_path_plugins
             waypoints_.pop_front();
 
             Eigen::Vector3d position(this->current_pose_x_,
-                                    this->current_pose_y_,
-                                    this->current_pose_z_);
+                                     this->current_pose_y_,
+                                     this->current_pose_z_);
             float distance_to_next_waypoint = (position - next_wayp).norm();
 
             // Check if goal is done
@@ -88,7 +125,7 @@ namespace follow_path_plugins
                 position = Eigen::Vector3d(this->current_pose_x_, this->current_pose_y_, this->current_pose_z_);
                 distance_to_next_waypoint = (position - next_wayp).norm();
 
-                if ( (distance_to_next_waypoint < goal_threshold_) && !waypoints_.empty() )
+                if ((distance_to_next_waypoint < goal_threshold_) && !waypoints_.empty())
                 {
                     next_wayp = waypoints_.front();
                     waypoints_.pop_front();
